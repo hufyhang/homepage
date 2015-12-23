a8e0a078d3a709d038126705fa5f4e9def4d5ecb
Feifei HANG <l.daniel.hung@gmail.com>
2015-12-23 13:36:58 +0800

    magical background

--- site/js/main.js
+++ site/js/main.js
@@ -8,12 +8,45 @@
   var $sections = $('section');
   var setHeight = function setHeight ($els, $target) {
     $els.forEach(function ($el) {
-      $el.css('height', $target.innerHeight())
-      .css('min-height', $target.innerHeight());
+      $el.css('height', $target.innerHeight() + 'px')
+      .css('min-height', $target.innerHeight() + 'px');
     });
   };
   setHeight([$header, $sections], $window);
   $window.on('resize', setHeight.bind(this, [$header, $sections], $window));
+
+  // Magic bg section.
+  var $greeting = $('section#greetings');
+  var $bgLeft = $greeting.find('#magic-bg-left');
+  var $bgRight = $greeting.find('#magic-bg-right');
+  var $avatarLeft = $greeting.find('#avatar-left');
+  var $avatarRight = $greeting.find('#avatar-right');
+
+  $bgLeft.css('width', $window.innerWidth() / 2 + 'px');
+  $bgRight.css('width', $window.innerWidth() + 'px');
+
+  var offset = 143 / 2;
+  $avatarLeft.css('left', $window.innerWidth() / 2 - offset + 'px');
+  $avatarRight.css('left', $window.innerWidth() / 2 - offset + 'px');
+
+  var $content = $greeting.find('#section-content');
+  var padding = 100;
+  var setMagicHeight = function setMagicHeight () {
+    var height = $content.innerHeight() + padding;
+    $greeting.css('height', height + 'px');
+    $bgLeft.css('height', height * 2 + 'px');
+    $bgRight.css('height', height * 2 + 'px');
+
+    $avatarLeft.css('left', $window.innerWidth() / 2 - offset + 'px');
+    $avatarRight.css('left', $window.innerWidth() / 2 - offset + 'px');
+  };
+  setMagicHeight();
+  $window.on('resize', setMagicHeight);
+
+  // Register magic bg event.
+  $greeting.on('mousemove', function (evt) {
+    $bgLeft.css('width', evt.clientX + 'px');
+  });
 
 
   // Set external links.
